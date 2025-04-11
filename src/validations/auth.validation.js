a9c915279d9dabe8ab69e0b563374ebcb94ac0e2
Lê Minh Bảo <besilent308@gmail.com>
2025-04-12 01:05:40 +0700

    feat(validation): Update register validation

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,23 +1,24 @@
 import {Joi} from 'express-validation';
 import ValidationConstant from "../constants/validation.constant.js";
+import S3Constant from "../constants/s3.constant.js";
 
 const authValidation = {
     register: {
         body: Joi.object({
-            first_name: Joi.string().optional(),
-            last_name: Joi.string().optional(),
+            first_name: Joi.string().optional().default("New"),
+            last_name: Joi.string().optional().default("User"),
             gender: Joi.string().valid(
                 ValidationConstant.GENDER.M,
                 ValidationConstant.GENDER.F
-            ).optional(),
-            date_of_birth: Joi.string().pattern(ValidationConstant.REGEX.DATE).optional()
+            ).optional().default(ValidationConstant.GENDER.M),
+            date_of_birth: Joi.string().pattern(ValidationConstant.REGEX.DATE).optional().default('01-01-1999')
                 .messages({'string.pattern.base': 'Date of birth must be in DD-MM-YYYY format'}),
             password: Joi.string().min(8).required()
                 .pattern(ValidationConstant.REGEX.PASSWORD)
                 .messages({
                     'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character'
                 }),
-            avatar_url: Joi.string().uri().optional(),
+            avatar_url: Joi.string().uri().optional().default(S3Constant.DEFAULT_USER_AVATAR_URL),
             phone_number: Joi.string().pattern(ValidationConstant.REGEX.PHONE).required()
                 .messages({'string.pattern.base': 'Phone number must be a valid international format'})
         })
